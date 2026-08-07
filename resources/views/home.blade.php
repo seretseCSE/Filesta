d008b263523b174ba9411be11dffb0d7d8195740
Ser <seretsefiresibhat@gmail.com>
2026-08-07 13:25:51 +0300

    finalized name

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('title', 'Finote Tsidik')
-@section('page-title', 'Finote Tsidik')
+@section('title', 'Filseta')
+@section('page-title', 'Filseta')
 
 @section('content')
     <div class="mx-auto flex min-h-dvh max-w-md flex-col px-4 pt-12">
-        <h1 class="text-2xl font-bold">Finote Tsidik</h1>
+        <h1 class="text-2xl font-bold">Filseta</h1>
         <p class="mt-2 text-gray-600">Your daily book of hours.</p>
     </div>
 @endsection
